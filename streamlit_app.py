24c45771bef8b653ae9ef3ff97cd4e4bdd023c1e
snowflaketrainingbage2 <141715407+snowflaketrainingbage2@users.noreply.github.com>
2023-08-09 15:33:23 +0530

    Update streamlit_app.py
    making use of pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit,pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +9,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

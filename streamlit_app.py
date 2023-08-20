71c1f7b2e6736415b473c6331c1966f5aa31833e
snowflaketrainingbage2 <141715407+snowflaketrainingbage2@users.noreply.github.com>
2023-08-20 17:56:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ def get_fruityvice_data(this_fruit_choice):
 #they Section to disolav trutoyce ani response
 streamlit. header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text input("What fruit would you like information about?")
-     if not fruit_choice:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+    if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
